563329d0ec41fd2e0efc2c0564035d33cdfd9ec2
Seol <131204758+JG-0204@users.noreply.github.com>
2023-10-22 16:21:20 +0800

    Add function for tab-switching

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import initialLoad from './page-load';
+import createHome from './home';
+import createMenu from './menu';
 
 const container = document.querySelector('#content');
 
 initialLoad(container);
+
+listenForTabSwitch();
+
+function listenForTabSwitch() {
+  const tabs = Array.from(document.querySelectorAll('.link'));
+  console.log(container.children);
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      if (tab.textContent === 'Home') switchTab(createHome());
+      if (tab.textContent === 'Menu') switchTab(createMenu());
+      if (tab.textContent === 'Contact') alert('Contact');
+    });
+  });
+}
+
+function switchTab(tab) {
+  const main = document.querySelector('main');
+  main.remove();
+  container.append(tab);
+}
